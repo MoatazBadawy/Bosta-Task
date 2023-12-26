ec7a12680d1667f0f79aaac0feb153c4fe50b099
MoatazBadawy <moataz.egy101@gmail.com>
2023-12-26 21:28:11 +0200

    feat: init NetworkModule and app di using hilt

---
+++ app/src/main/java/com/moataz/bosta_task/app/BostaApplication.kt
@@ -0,0 +1,7 @@
+package com.moataz.bosta_task.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BostaApplication : Application()
---
+++ app/src/main/java/com/moataz/bosta_task/app/di/NetworkModule.kt
@@ -0,0 +1,55 @@
+package com.moataz.bosta_task.app.di
+
+import com.moataz.bosta_task.BuildConfig
+import com.moataz.bosta_task.profile.ProfileService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideProfileNetworkService(
+        retrofit: Retrofit,
+    ): ProfileService {
+        return retrofit.create(ProfileService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient()
+            .newBuilder()
+            .callTimeout(25, TimeUnit.SECONDS)
+            .connectTimeout(25, TimeUnit.SECONDS)
+        return builder.build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+}
